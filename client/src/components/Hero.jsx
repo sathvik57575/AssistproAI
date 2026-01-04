360e10eafe44ce15d2afe5440b142f7316ecf026
katta sathvik <f20200191@hyderabad.bits-pilani.ac.in>
2026-01-04 23:15:51 +0530

    added-demo-video

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
 
           <div className='mt-4 flex flex-wrap justify-center gap-4 text-sm max-sm:text-xs'>
               <button onClick={() => navigate('/ai')} className='bg-primary text-white px-10 py-3 rounded-lg hover:scale-102 active:scale-95 transition cursor-pointer'>Start creating now</button>
-              <button className='bg-white px-10 py-3 rounded-lg border border-gray-300 hover:scale-102 active:scale-95 transition cursor-pointer'>Watch demo</button>
+              <a href='/demo.mp4' className='bg-white px-10 py-3 rounded-lg border border-gray-300 hover:scale-102 active:scale-95 transition cursor-pointer'>Watch demo</a>
           </div>
 
           <div className='flex items-center gap-4 mt-8 mx-auto text-gray-600'>
